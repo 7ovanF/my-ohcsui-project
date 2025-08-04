5166c18f549015d8eda77b0e9664a4a534633d81
Jovan F <jovanfinesta@gmail.com>
2025-08-04 08:34:25 +0700

    [DAY 2] this repo is cooked ong (im changing projects)

--- src/pages/AboutMe.jsx
+++ src/pages/AboutMe.jsx
@@ -7,8 +7,11 @@ function AboutMe() {
     // TODO: mgkin cari cara biar ada video yg light, kalau iya, videonya fade from black setelah load 
     <>
       <h3>About Me</h3>
-      <p>Saya Jovan Finesta, mahasiswa baru di Jurusan Ilkom UI. Saya sudah memiliki pengalaman dalam membangun website dengan bahasa PHP (dengan frontend HTML, CSS, dan Javascript) serta menghubungkan ke database MySQL. Walau, saya kira PHP dan MySQL tidak akan digunakan samsek...</p>
-      <p>Website ini dibangun tanpa pengalaman ReactJS dan TailwindCSS sama sekali, dan baru dimulai tepat pada hari akhir pendaftaran Open House Fasilkom (terima kasih atas extension deadline-nya!!!) Jadi, jika terdapat permasalahan dengan struktur yang membingungkan / penuh dengan <i>bad practices</i>, saya minta maaf sebelumnya. Dan jika saya diterima, saya sebagai pemula meminta bimbingan kating/teman2 yang lebih berpengalaman 🙏</p>
+      <p>Saya <b>Jovan Finesta</b>, mahasiswa baru di Jurusan Ilkom UI tahun 2025. Saya sudah memiliki pengalaman dalam membangun website dengan bahasa PHP (dengan frontend Javascript dan database MySQL). Saya bekerja dengan beberapa programmer lain di <a href="https://teraluxliving.com" target="blank_">PT Selva Indonesia</a> sehingga saya memiliki dasar-dasar kerjasama.</p>
+      <p>Website ini dibangun <b>tanpa pengalaman ReactJS dan TailwindCSS sama sekali</b>, dan baru dimulai tepat pada hari akhir pendaftaran Open House Fasilkom 2025 (terima kasih atas extension <i>deadline</i>-nya!!!) Jadi, jika terdapat permasalahan dengan struktur yang membingungkan / penuh dengan <i>bad practices</i>, saya minta maaf sebelumnya. Dan jika saya diterima sebagai bagian dari OH Fasilkom, <b>saya sebagai pemula meminta bimbingan kating/teman2 yang lebih berpengalaman 🙏</b></p>
+      <h4>Kenapa OH Fasilkom 2025?</h4>
+      <p>Awalnya, saya ingin membangun relasi yang kuat dengan sesama programmer (gak ecek2 kenalan doang) sekalian menambah pengalaman bekerja dengan orang2 yang baru dikenal.</p>
+      <p>Sejujurnya, saya hampir tidak jadi mendaftar karena permintaan yang sangat sulit, yaitu membangun website baru dengan ReactJS, namun melihat pengumuman ekstensi waktu pendaftaran, saya merasa "terpanggil" dan perlu untuk minimal menyoba.</p>
       
     </>
   )
--- src/pages/Intro.jsx
+++ src/pages/Intro.jsx
@@ -6,10 +6,11 @@ function Intro() {
   return (
     // TODO: mgkin cari cara biar ada video yg light, kalau iya, videonya fade from black setelah load 
     <>
-      <h4>Halo, nama saya</h4>
-      <h1>Jovan Finesta</h1>
-      <p>Newbie Web Dev, (a bit) experienced in both Frontend and Backend</p>   
-      
+      <div className="sm:container bg-blue-100 mx-auto">
+        <h4>Halo, nama saya</h4>
+        <h1>Jovan Finesta</h1>
+        <p>Newbie Web Dev, (a bit) experienced in both Frontend and Backend</p>   
+      </div>
     </>
   )
 }
